3638cf28757723690d6712a8f0fa97a185b5c5d8
Abhishek <naikabhishek350@gmail.com>
2022-03-08 19:36:10 +0530

    Connected to moviesdb database using mongoose

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const db = require('./models/index');
 
 const port = 9000;
 
@@ -25,4 +26,18 @@ const app = http.createServer((request, response) => {
     }
 });
 
-app.listen(port);
+db.mongoose.connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+.then(() => {
+    console.log("Connected to the database!");
+})
+.catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+});
+
+app.listen(port, () => {
+    `App started at port ${port}`
+});
